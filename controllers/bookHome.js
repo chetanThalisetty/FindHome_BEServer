cda7fb81c7c9d983e732c857854cf2e636cc7326
Nikhil497 <33mnikhilkumar@gmail.com>
2017-12-11 20:29:25 -0500

    implemented logout,error handling for empty search, booking page

--- controllers/bookHome.js
+++ controllers/bookHome.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const logger = require('../lib/logger');
 const bookingModel = require('../models/bookingModel');
-
+let server_config = require('../config/server_config');
 /**
  * @author Chetan Sai Kumar Thalisetty [tchetan1@umbc.edu]
  */
@@ -18,7 +18,7 @@ router.get('/',function(req, res, next){
     console.log('user trying to book for homes: %j', req.body);
     bookingModel.bookHome(req,(result) => {
         console.log(JSON.parse(JSON.stringify(result)));
-        res.send(result);
+        res.render('booking', {data: result, host_address: server_config.hosting_server_ip, port: server_config.port_no});
     });
 });
 
